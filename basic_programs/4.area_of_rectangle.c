6df78ff1ed66a49bcb5fb0bdcad4556bcd93cdec
Mrigank005 <mriganksingh2000@gmail.com>
2024-10-27 14:48:23 +0530

    Update 4.area_of_rectangle.c

--- basic_programs/4.area_of_rectangle.c
+++ basic_programs/4.area_of_rectangle.c
@@ -4,8 +4,8 @@ int main()
 	int height = 7;
 	int width = 5;
   	int perimeter = 2*(height + width);
-	printf("Perimeter of the rectangle = %d inches\n", perimeter);
+	printf("Perimeter of the rectangle = %d cm\n", perimeter);
 	int area = height * width;
-	printf("Area of the rectangle = %d square inches\n", area);
+	printf("Area of the rectangle = %d square cm\n", area);
 	return(0);
 }

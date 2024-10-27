17b4d848865263904a0e93f8d99a76ca628aedef
Mrigank005 <mriganksingh2000@gmail.com>
2024-10-27 14:40:04 +0530

    Create 3.reverse_char.c
    Write a C program to print the characters in reverse.

---
+++ basic_programs/3.reverse_char.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() 
+{
+    char char1 = 'B';
+    char char2 = 'M';
+    char char3 = 'W';
+    printf("The reverse of %c%c%c is %c%c%c\n", char1, char2, char3, char3, char2, char1);
+}

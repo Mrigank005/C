88c755e551856b37fee0dc494c4a19c20344d93d
Mrigank005 <mriganksingh2000@gmail.com>
2024-10-27 14:49:22 +0530

    Update 5.area_of_circle.c

--- basic_programs/5.area_of_circle.c
+++ basic_programs/5.area_of_circle.c
@@ -4,7 +4,7 @@ int main()
   int radius = 6;
   float perimeter = 2 * 3.14 * radius;
   printf("Perimeter of the Circle = %f cm\n", perimeter);
-	float area = 3.14 * radius * radius;
+  float area = 3.14 * radius * radius;
   printf("Area of the Circle = %f square cm\n", area);
   return(0);
 }

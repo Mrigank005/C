7de0179112c7bbf734c7a89e932a3ce15e94208d
Mrigank005 <mriganksingh2000@gmail.com>
2024-10-27 14:47:57 +0530

    Create 5.area_of_circle.c
    Write a C program to compute the perimeter and area of ​​a circle with a given radius.

---
+++ basic_programs/5.area_of_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+  int radius = 6;
+  float perimeter = 2 * 3.14 * radius;
+  printf("Perimeter of the Circle = %f cm\n", perimeter);
+	float area = 3.14 * radius * radius;
+  printf("Area of the Circle = %f square cm\n", area);
+  return(0);
+}
